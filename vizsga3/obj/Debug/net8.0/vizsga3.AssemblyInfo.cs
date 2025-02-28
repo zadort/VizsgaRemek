cf95f103945c303a63e70b113909b2db07dcc6ef
zadort <zadort@kkszki.hu>
2025-02-28 12:05:52 +0100

    Other small upgrades and corrections

--- vizsga3/obj/Debug/net8.0/vizsga3.AssemblyInfo.cs
+++ vizsga3/obj/Debug/net8.0/vizsga3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vizsga3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d14cc142bf70dc0017098360a349f405982fa90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0564176c58399c74052bc926717721be260d2c49")]
 [assembly: System.Reflection.AssemblyProductAttribute("vizsga3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vizsga3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
