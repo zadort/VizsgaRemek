ab7cab4d8672db88875cd5dca9b0f9b07e623b50
zadort <zadort@kkszki.hu>
2025-04-01 10:15:56 +0200

    Email system changed

--- vizsga3/obj/Debug/net8.0/vizsga3.AssemblyInfo.cs
+++ vizsga3/obj/Debug/net8.0/vizsga3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vizsga3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a990283d07af01068dac6e368499ca37a45e4a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2a08013c507d79974b26bac6de4268a59681d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("vizsga3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vizsga3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
